a4443c66afd45f80a2d8c22a1d508eedcc5fdc75
vietanhlee <levietanhtrump@gmail.com>
2025-05-14 16:39:22 +0700

    up

--- huffman.py
+++ huffman.py
@@ -9,23 +9,33 @@ class HuffmanNode:
         self.left = left # Node trái
         self.right = right # Node phải
 
+    # Định nghĩa thuộc tính này phục vụ cho việc so sánh trong pq
     def __lt__(self, other):
         return self.freq < other.freq
 
 class HuffmanCoding:
     def __init__(self):
+        # Dict tra code nén 
+        """
+        codes = {
+                bytes: code
+            }
+            
+            reverse_mapping thì ngược lại
+        """
         self.codes = {}
+        # Dict tra code giải nén
         self.reverse_mapping = {}
 
-    # Tạo tần số 
-    def build_frequency_dict(self, data_bytes):
+    def build_frequency_dict(self, data_bytes : list) -> dict:
+        """Tạo tần số theo bytes"""
         freq = defaultdict(int)
         for b in data_bytes:
             freq[b] += 1
         return freq
-
-    # Build priority queue chứa các node 
-    def build_heap(self, freq_dict):
+ 
+    def build_heap(self, freq_dict : dict) -> list[HuffmanNode]:
+        """Build priority queue chứa các node"""
         heap = []
         for byte, frequency in freq_dict.items():
             # Tạo các node lá (chứa các byte)
@@ -33,49 +43,53 @@ class HuffmanCoding:
             heapq.heappush(heap, node)
         return heap
 
-    # Gộp từng node nhỏ nhất lại thành 1 
-    def merge_nodes(self, heap):
+    def merge_nodes_huffman(self, heap : list[HuffmanNode]) -> list:
+        """Gộp từng 2 node nhỏ nhất lại thành 1 """
         while len(heap) > 1:
             node1 = heapq.heappop(heap)
             node2 = heapq.heappop(heap)
             
             # Node nhỏ bên trái node lớn hơn bên phải
             merged = HuffmanNode(node1.freq + node2.freq, left=node1, right=node2)
+            # Đưa vào pq
             heapq.heappush(heap, merged)
         return heap
 
-    # Tạo chỉ số các nhánh    
-    def make_codes_helper(self, node, current_code):
+    def func_make_code_from_node(self, node : HuffmanNode, current_code: str) -> None:
+        """Tạo code cho các node từ một node start"""
         # Điều kiện dừng nếu node truyền vào là None
         if node is None:
             return
         
-        # Điều kiện dừng khi gặp node lá    
+        # Điều kiện dừng khi gặp node lá tức bytes ban đầu ta cần    
         if node.byte is not None:
             self.codes[node.byte] = current_code
             self.reverse_mapping[current_code] = node.byte
             return
         
-        # Gọi đệ quy
-        self.make_codes_helper(node.left, current_code + "0")
-        self.make_codes_helper(node.right, current_code + "1")
+        # Gọi đệ quy đến 2 nhánh sau
+        self.func_make_code_from_node(node.left, current_code + "0")
+        self.func_make_code_from_node(node.right, current_code + "1")
 
-    # Lệnh gọi tạo các mã huffman
-    def make_codes(self, heap):
+    def make_codes_huffman_from_root(self, heap : HuffmanNode) -> None:
+        """Tạo mã huffman từ root đầu vào là pq chứa 1 huffmannode duy nhất
+
+Gọi hàm này sau khi gộp hết các node huffman"""
         root = heapq.heappop(heap)
-        self.make_codes_helper(root, "")
+        self.func_make_code_from_node(root, "")
 
-    
-    def get_encoded_data(self, data_bytes):
-        encoded_bits = "".join(self.codes[b] for b in data_bytes)
+    def get_encoded_data(self, data_bytes : list) -> str:
+        """Mã hoá data
+        
+Đầu ra 8 bit đầu chứa số bit padding"""
+        encoded_bits = "".join(self.codes[bytes] for bytes in data_bytes)
         extra_padding = 8 - len(encoded_bits) % 8
         encoded_bits += "0" * extra_padding
         padded_info = f"{extra_padding:08b}"
+        
         return padded_info + encoded_bits
 
-    def get_byte_array(self, padded_encoded_bits):
-        if len(padded_encoded_bits) % 8 != 0:
-            raise ValueError("Encoded bits length is not padded to full bytes.")
+    def get_byte_array(self, padded_encoded_bits : str) -> bytearray:
         return bytearray(int(padded_encoded_bits[i:i+8], 2) for i in range(0, len(padded_encoded_bits), 8))
 
     def compress(self, input_path : str, output_path : str):
@@ -85,24 +99,29 @@ class HuffmanCoding:
 
         print('Đã đọc xong file')
         
+        # Các thủ tục tạo data
         freq_dict = self.build_frequency_dict(data)
         heap = self.build_heap(freq_dict)
-        heap = self.merge_nodes(heap)
-        self.make_codes(list(heap))
+        heap = self.merge_nodes_huffman(heap)
+        # Lấy data cho 2 dict code và mapping_reverse
+        self.make_codes_huffman_from_root(list(heap))
 
         print('Đã tạo xong code')
         
+        # Tạo bytes array chứa data (1 byte = 8 bit)
         padded_encoded_bits = self.get_encoded_data(data)
         b_arr = self.get_byte_array(padded_encoded_bits)
 
         with open(output_path, 'wb') as output:
-            # Ghi thông tin bảng mã nén
+            # Ghi thông tin bảng mã nén  Byte gốc	Độ dài mã	Mã code mã hoá dạng byte
             mapping_size = len(self.codes)
             output.write(mapping_size.to_bytes(2, 'big'))
             for byte, code in self.codes.items():
                 output.write(bytes([byte]))
+                
                 code_length = len(code)
                 output.write(bytes([code_length]))
+                
                 output.write(int(code, 2).to_bytes((code_length + 7) // 8, 'big'))
             
             # Ghi mã nén
